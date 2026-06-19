eba99c470c12c2184332eabae79b90d9cc12858a
pavithra9059 <pavithraragulapadu@gmail.com>
2026-06-19 18:10:54 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,26 @@
+import StudentForm
+from "./components/StudentForm";
+
+import StudentList
+from "./components/StudentList";
+
+function App() {
+
+ return (
+
+ <div>
+
+ <h1>
+ MERN Student Management
+ </h1>
+
+ <StudentForm />
+
+ <StudentList />
+
+ </div>
+
+ );
+}
+
+export default App;
